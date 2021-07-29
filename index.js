eb3bf1956b9fa5fc100c686b341a340d64dcbb7e
willian pereira <williansouza9687a@gmail.com>
2021-07-29 17:39:32 -0300

    quase pronto

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ busca.addEventListener('submit', async (event)=>{
     }
 
         
-    mapboxgl.accessToken = 'sk.eyJ1IjoiZ290d2lsbCIsImEiOiJja3JwYXJxZXcyYmRkMzJrZHJvYmZ6a3djIn0.ODX_145CABmA7F_UXwlI3w';
-    const  map = new mapboxgl.Map({
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZ290d2lsbCIsImEiOiJja2tzb2lseG0wYnZzMnVxZmp2aHRrMmpmIn0.TwBtDLsSy_aw1RVaef1Sew';
+        var map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v9', // style URL
-        center: [ -0.077, 51.533 ], // starting position [lng, lat]
-        zoom: 12 // starting zoom
-    });
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+        });
         
     new mapboxgl.Marker()
     .setLngLat([ -0.077, 51.533 ])
